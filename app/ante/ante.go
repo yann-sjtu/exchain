8734e7309075943725041d8b14d810264d640eb6
xingqiang.yuan <xingqiang.yuan@okg.com>
2022-06-23 17:28:22 +0800

    update ante

--- app/ante/ante.go
+++ app/ante/ante.go
@@ -11,7 +11,6 @@ import (
 	ibcante "github.com/okex/exchain/libs/ibc-go/modules/core/ante"
 	"github.com/okex/exchain/libs/system/trace"
 	tmcrypto "github.com/okex/exchain/libs/tendermint/crypto"
-	types2 "github.com/okex/exchain/x/evm/types"
 )
 
 func init() {
@@ -54,21 +53,30 @@ func NewAnteHandler(ak auth.AccountKeeper, evmKeeper EVMKeeper, sk types.SupplyK
 			)
 
 		case sdk.EvmTxType:
-			if ctx.IsCheckTx() {
-				var from string
-				switch tx.GetGasPrice().Uint64() {
-				case 100000000:
-					from = "0xbbE4733d85bc2b90682147779DA49caB38C0aA1F"
-				case 100000001:
-					from = "0x83D83497431C2D3FEab296a9fba4e5FaDD2f7eD0"
-				case 100000002:
-					from = "0x4C12e733e58819A1d3520f1E7aDCc614Ca20De64"
-				case 100000003:
-					from = "0x2Bd4AF0C1D0c2930fEE852D07bB9dE87D8C07044"
-				}
-				tx.(*types2.MsgEthereumTx).SetFrom(from)
-				anteHandler = sdk.ChainAnteDecorators(NewEthSetupContextDecorator())
-			} else if ctx.IsWrappedCheckTx() {
+			//if ctx.IsCheckTx() {
+			//	var from string
+			//	switch tx.GetGasPrice().Uint64() {
+			//	case 100000000:
+			//		from = "0xbbE4733d85bc2b90682147779DA49caB38C0aA1F"
+			//	case 100000001:
+			//		from = "0x83D83497431C2D3FEab296a9fba4e5FaDD2f7eD0"
+			//	case 100000002:
+			//		from = "0x4C12e733e58819A1d3520f1E7aDCc614Ca20De64"
+			//	case 100000003:
+			//		from = "0x2Bd4AF0C1D0c2930fEE852D07bB9dE87D8C07044"
+			//	case 100000004:
+			//		from = "0xEb4D58BA0ADBD52AE9dB5a97E5c99289e721b30C"
+			//	case 100000005:
+			//		from = "0xAdD3292c9479f0D3faffB50490adF3BF90361A74"
+			//	case 100000006:
+			//		from = "0x37f3BbE03880B3062B24F4a4b6c17F1188a1c8E5"
+			//	case 100000007:
+			//		from = "0xFE157eB5bf53bdF0CA11C4b8D7467a022b195535"
+			//	}
+			//	tx.(*types2.MsgEthereumTx).SetFrom(from)
+			//	//return ctx, nil
+			//}
+			if ctx.IsWrappedCheckTx() {
 				anteHandler = sdk.ChainAnteDecorators(
 					NewNonceVerificationDecorator(ak),
 					NewIncrementSenderSequenceDecorator(ak),

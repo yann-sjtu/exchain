1b76ee71ea3624b1756a5eaa795d60c7e40c1e60
xingqiang.yuan <xingqiang.yuan@okg.com>
2022-06-23 17:26:49 +0800

    add four more genesis addresses

--- dev/local-perf.sh
+++ dev/local-perf.sh
@@ -28,10 +28,11 @@ run() {
       --log_level $LOG_LEVEL \
       --log_file json \
       --enable-dynamic-gp=false \
-      --consensus.timeout_commit 100ms \
+      --mempool.sort_tx_by_gp=true \
+      --consensus.timeout_commit 50ms \
       --disable-abci-query-mutex=true \
       --mempool.max_tx_num_per_block=10000 \
-      --mempool.size=20000 \
+      --mempool.size=100000 \
       --local_perf=tx \
       --enable-preruntx=false \
       --iavl-enable-async-commit \
@@ -91,6 +92,24 @@ exchaincli keys add --recover admin17 -m "antique onion adult slot sad dizzy sur
 
 exchaincli keys add --recover admin18 -m "lazy cause kite fence gravity regret visa fuel tone clerk motor rent" -y
 
+# 0xEb4D58BA0ADBD52AE9dB5a97E5c99289e721b30C
+# afbf867c175d639c46199954f8f3c126f00a45c232089d9ce72b3555ebf6d02d
+exchaincli keys add --recover admin19 -m "solid dinner float window exile lend comfort thunder tourist argue retreat edge" -y
+
+# 0xAdD3292c9479f0D3faffB50490adF3BF90361A74
+# b97487ce7527f6f638b475936edacda3e7365f394d3a453a9e16a8c71de126fd
+exchaincli keys add --recover admin20 -m "asthma sea appear divorce someone awkward behave swamp candy record squeeze north" -y
+
+# 0x37f3BbE03880B3062B24F4a4b6c17F1188a1c8E5
+# 424359a24b08bb6f1bd63851299be67c813dc2cf295e42d27b8bcf7a887af8dd
+exchaincli keys add --recover admin21 -m "midnight mistake there culture idea speed brisk tunnel legend sniff excuse anger" -y
+
+# 0xFE157eB5bf53bdF0CA11C4b8D7467a022b195535
+# a1fa94c958dfaa60f395ad334bdce3997269e828e5b4b90cecf94b3e01b1647b
+exchaincli keys add --recover admin22 -m "dove tobacco token resemble near rate please sheriff priority sure twin tag" -y
+
+
+#exchaincli keys add --recover admin19 -m "" -y
 # Set moniker and chain-id for Ethermint (Moniker can be anything, chain-id must be an integer)
 exchaind init $MONIKER --chain-id $CHAINID --home $HOME_SERVER
 
@@ -117,6 +136,11 @@ exchaind add-genesis-account $(exchaincli keys show $KEY    -a) 100000000okt --h
 exchaind add-genesis-account $(exchaincli keys show admin16 -a) 900000000okt --home $HOME_SERVER
 exchaind add-genesis-account $(exchaincli keys show admin17 -a) 900000000okt --home $HOME_SERVER
 exchaind add-genesis-account $(exchaincli keys show admin18 -a) 900000000okt --home $HOME_SERVER
+exchaind add-genesis-account $(exchaincli keys show admin19 -a) 900000000okt --home $HOME_SERVER
+exchaind add-genesis-account $(exchaincli keys show admin20 -a) 900000000okt --home $HOME_SERVER
+exchaind add-genesis-account $(exchaincli keys show admin21 -a) 900000000okt --home $HOME_SERVER
+exchaind add-genesis-account $(exchaincli keys show admin22 -a) 900000000okt --home $HOME_SERVER
+
 
 # Sign genesis transaction
 exchaind gentx --name $KEY --keyring-backend test --home $HOME_SERVER
